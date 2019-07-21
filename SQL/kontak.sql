3cb1f7def43ec39390d0ed2211bf511762bd53f9
Rahmat Hidayat <rh.hie.dayatt@gmail.com>
2019-07-21 17:34:00 +0700

    edit kontak.sql

--- SQL/kontak.sql
+++ SQL/kontak.sql
@@ -8,3 +8,10 @@ nama STRING(25),
 alamat STRING(50),
 telp STRING(20)
 );
+
+INSERT INTO kontak (nama, alamat, telp) VALUES ('Andi','Sleman','085123789');
+INSERT INTO kontak (nama, alamat, telp) VALUES ('Indah','Klaten','08513513');
+INSERT INTO kontak (nama, alamat, telp) VALUES ('Bagas','Bantul','08651314');
+
+
+
